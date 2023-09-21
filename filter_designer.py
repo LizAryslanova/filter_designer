05c49107bf35a6d874a2336c930145efeaad0bba
Liz Aryslanova <connect.with.liza@gmail.com>
2023-09-21 19:38:05 +0400

    Adds symmetric filter, attack filter, release filter, classic ballistic filter

--- filter_designer.py
+++ filter_designer.py
@@ -26,8 +26,11 @@ for i in range(200):
     =======================
 '''
 
-alpha = 0.95
-beta = 0.988
+a = 0.8
+b = 0.2
+g = 0.9
+d_a = 0.9
+d_r = 0.1
 
 
 '''
@@ -36,22 +39,41 @@ beta = 0.988
     =======================
 '''
 
-def filter_alpha(input_signal, alpha):
+def symmetric_filter(input_signal, alpha):
     y = np.zeros(len(input_signal))
     for i in range(len(input_signal) - 1):
         y[i+1] = input_signal[i+1] + alpha * (y[i] - input_signal[i+1])
     return y
 
 
-def filter_beta(input_signal, beta):
+def release_filter(input_signal, alpha):
     y = np.zeros(len(input_signal))
     for i in range(len(input_signal) - 1):
-        y[i+1] = max(input_signal[i+1], beta * y[i])
+        y[i+1] = input_signal[i+1] + max(0, alpha * (y[i] - input_signal[i+1]))
+    return y
+
+def attack_filter(input_signal, alpha):
+    y = np.zeros(len(input_signal))
+    for i in range(len(input_signal) - 1):
+        y[i+1] = input_signal[i+1] + min(0, alpha * (y[i] - input_signal[i+1]))
+    return y
+
+
+def c_ballistic_filter(input_signal, alpha_attack, alpha_release):
+    y = np.zeros(len(input_signal))
+    for i in range(len(input_signal) - 1):
+        if input_signal[i+1] <= y[i]:
+            alpha = alpha_attack
+        else:
+            alpha = alpha_release
+
+        y[i+1] = input_signal[i+1] + alpha * (y[i] - input_signal[i+1])
     return y
 
 
 
-output_signal = filter_alpha(filter_beta(input_signal, beta), alpha)
+
+output_signal = symmetric_filter(attack_filter(c_ballistic_filter(input_signal, d_a, d_r), g), a)
 
 # plot images: input signal + filtered signal
 plt.plot(input_signal, color='g', label='Input')
